17f58f7ef9b3a8a4204ff11bd873793d3bf2a1e2
Delancey <dsdelancey@gmail.com>
2020-06-01 11:24:12 -0400

    remove limit on numbers provided in string

--- Calculator.java
+++ Calculator.java
@@ -2,17 +2,17 @@ public class Calculator {
     public static final int add(final String numbers){
         int returnValue = 0;
         String[] numbersArray = numbers.split(",");
-        if(numbersArray.length >2){
-            throw new RuntimeException("Up to 2 numbers separated by a comma (, ) are allowed");
-        } else {
+        // if(numbersArray.length >2){
+        //     throw new RuntimeException("Up to 2 numbers separated by a comma (, ) are allowed");
+        // } 
             //go through each number in array and convert to integer
-            for (String number: numbersArray){
-                if(!number.isEmpty()){
-                    //converts number String to int, adds returnValue
-                    returnValue += Integer.parseInt(number);
-                //if not possible throw exception
-                }                 
-            }            
+        for (String number: numbersArray){
+            if(!number.isEmpty()){
+                //converts number String to int, adds returnValue
+                returnValue += Integer.parseInt(number);
+            //if not possible throw exception
+            }                 
+                   
         }
         return returnValue;
         //returns 0(default value) if no items in array
--- CalculatorTest.java
+++ CalculatorTest.java
@@ -5,11 +5,18 @@ import org.junit.Test;
 public class CalculatorTest {
 
     //use annotation when you're expecting a Runtime Exception, not any return value
-    @Test(expected = RuntimeException.class)
-    public final void threeOrMoreNumbersThrowsException(){
-        Calculator.add("1,2,3");
+    // @Test(expected = RuntimeException.class)
+    // public final void threeOrMoreNumbersThrowsException(){
+    //     Calculator.add("1,2,3");
+
+    // }
+
+    @Test
+    public final void anyNumberofValuesUsedReturnSums(){
+        Assert.assertEquals(1+2+3+4+5+6+7, Calculator.add("1,2,3,4,5,6,7"));
 
     }
+
 
     @Test
     public final void twoNumbersUsedNoExceptionThrown(){
